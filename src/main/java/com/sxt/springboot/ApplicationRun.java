729ede99512afeca7f714f194cf9ddbb83efb9c8
soutong <tss139@163.com>
2018-11-14 08:08:01 +0800

    first commit

---
+++ src/main/java/com/sxt/springboot/ApplicationRun.java
@@ -0,0 +1,19 @@
+package com.sxt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lenovo on 2018/8/20.
+ */
+@SpringBootApplication
+public class ApplicationRun
+{
+   public static void main(String args[]){
+       //第一种，直接启动
+       SpringApplication.run(ApplicationRun.class,args);
+       //第二种，自定义banner图，在resources文件夹下新建banner图片
+
+
+   }
+}
