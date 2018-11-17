9f0c4002672d381d013742096de027e4a47e7ded
soutong <tss139@163.com>
2018-11-17 23:21:19 +0800

    第四次提交

--- src/main/java/com/sxt/springboot/ApplicationRun.java
+++ src/main/java/com/sxt/springboot/ApplicationRun.java
@@ -13,9 +13,7 @@ public class ApplicationRun
        //第一种，直接启动
        SpringApplication.run(ApplicationRun.class,args);
        //第二种，自定义banner图，在resources文件夹下新建banner图片
-        //第三种
-       System.out.println("xiaogou");
+
 
    }
-
 }
